a380922bc79f36b1d8188111c6147fc4f1e737d2
GeorgeSikora <jirisikoracz@email.cz>
2021-02-15 23:30:21 +0100

    skládání options pro select jazyků z DB

--- library/language.php
+++ library/language.php
@@ -8,12 +8,29 @@ function getLanguageOptions() {
         $id = $lang['id'];
         $name = $lang['name'];
 
-        
         echo '<option ';
         echo 'value="'. $id .'"';
         echo '>';
 
         echo $name;
+
+        echo '</option>';
+    }
+}
+
+function getWordOptions() {
+
+    $words = SQL("SELECT * FROM words");
+
+    foreach ($words as $word) {
+        $id = $word['id'];
+        $word = $word['word'];
+
+        echo '<option ';
+        echo 'value="'. $id .'"';
+        echo '>';
+
+        echo $word;
 
         echo '</option>';
     }
